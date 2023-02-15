9b984d7d1d69851387789849af09a3b7d0bfc3ee
Tarook <Tarook.Zinou@gmail.com>
2023-02-15 16:02:57 +0100

    Parseur fonctionnel

--- src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_joueur_sme/entities/bo/FichierQuestionnaireBO.java
+++ src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_joueur_sme/entities/bo/FichierQuestionnaireBO.java
@@ -3,21 +3,30 @@ package fr.iut.montreuil.S04_R02_2023_1_ButtonBash_joueur_sme.entities.bo;
 import com.opencsv.CSVReader;
 
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class FichierQuestionnaireBO {
     private String nomFichier; // files must be placed in src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_joueur_sme/resources
+    private ArrayList<String[]> rawQuestions = new ArrayList<>();
 
     public FichierQuestionnaireBO(String nomFichier) {
         this.nomFichier = nomFichier;
+
         try {
             CSVReader reader = new CSVReader(new FileReader("src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_joueur_sme/resources/" + nomFichier));
             String[] nextLine;
             while ((nextLine = reader.readNext()) != null) {
                 // nextLine[] is an array of values from the line
-                System.out.println(nextLine);
+                rawQuestions.add(nextLine);
             }
         } catch (Exception e) {
             System.out.println("Fichier non trouve : " + e.getMessage());
+        }
+        for(String[] s : rawQuestions) {
+            for(String s2 : s) {
+                System.out.print(s2 + " ");
+            }
+            System.out.println();
         }
     }
 
---
+++ src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_joueur_sme/entities/dto/QuestionnaireDTO.java
@@ -0,0 +1,5 @@
+package fr.iut.montreuil.S04_R02_2023_1_ButtonBash_joueur_sme.entities.dto;
+
+public class QuestionnaireDTO {
+    
+}
