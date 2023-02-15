a942f2b81b54fc88477e4023a14ade2b5b432646
Tarook <Tarook.Zinou@gmail.com>
2023-02-15 13:56:24 +0100

    Ajout fichier et classe avec parseur csv

---
+++ src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_joueur_sme/entities/bo/FichierQuestionnaireBO.java
@@ -0,0 +1,30 @@
+package fr.iut.montreuil.S04_R02_2023_1_ButtonBash_joueur_sme.entities.bo;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.nio.file.Path;
+
+public class FichierQuestionnaireBO {
+    private String nomFichier; // files must be placed in src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_joueur_sme/resources
+
+    public FichierQuestionnaireBO(String nomFichier) {
+        // TODO Auto-generated constructor stub
+        this.nomFichier = nomFichier;
+        try {
+            CSVReader reader = new CSVReader(new FileReader("src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_joueur_sme/resources/" + nomFichier));
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                // nextLine[] is an array of values from the line
+                System.out.println(nextLine);
+            }
+        } catch (Exception e) {
+            System.out.println("Fichier non trouve : " + e.getMessage());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        FichierQuestionnaireBO fichierQuestionnaireBO = new FichierQuestionnaireBO("questionsQuizz_V1.1.csv");
+    }
+}
