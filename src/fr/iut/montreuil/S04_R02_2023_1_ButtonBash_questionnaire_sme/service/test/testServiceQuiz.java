6e505f0059d7514dc4fc0497dea160cf5803eb62
Théo Miskovic <theosyle42@gmail.com>
2023-03-16 13:53:47 +0100

    ajout de cas de tests

--- src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_questionnaire_sme/service/test/testServiceQuiz.java
+++ src/fr/iut/montreuil/S04_R02_2023_1_ButtonBash_questionnaire_sme/service/test/testServiceQuiz.java
@@ -1,16 +1,50 @@
 package fr.iut.montreuil.S04_R02_2023_1_ButtonBash_questionnaire_sme.service.test;
 
-import org.junit.jupiter.api.BeforeEach;
+import fr.iut.montreuil.S04_R02_2023_1_ButtonBash_questionnaire_sme.service.entities.bo.FichierQuestionnairesBO;
+import fr.iut.montreuil.S04_R02_2023_1_ButtonBash_questionnaire_sme.service.impl.ServiceQuiz;
+//import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class testServiceQuiz {
-    @BeforeEach
-    public void init(){
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.util.ArrayList;
+
+
+public class testServiceQuiz {
+
+
+//    @BeforeEach
+//    public void init(){
+//        FichierQuestionnairesBO bo = new FichierQuestionnairesBO("questionsQuizz_V1.1.csv", ';');
+//    }
+
+    @Test
+    public void testListQuestionnaireNotNull(){
+        FichierQuestionnairesBO bo = new FichierQuestionnairesBO("questionsQuizz_V1.1.csv", ';');
+        ArrayList<ArrayList<String[]>> rawQuestions = bo.getRawQuestionnaires();
+        for (ArrayList<String[]> listes  :rawQuestions){
+            for (String[] tab : listes) {
+                for (String s : tab) {
+                    assertNotEquals(s.length(), 0);
+                }
+            }
+        }
     }
 
     @Test
-    public void testListQuestionnaire(){
+    public void testAjouterQuestionnaire (){
+        FichierQuestionnairesBO bo = new FichierQuestionnairesBO("questionsQuizz_V1.1.csv", ';');
+        ServiceQuiz serv = new ServiceQuiz(bo);
+        serv.ajouterQuestionnaire(bo,1);
+        assertEquals(2,serv.fournirListeQuestionnaire().size());
+    }
 
+    @Test
+    public void testFournirUnQuestionnaire (){
+        FichierQuestionnairesBO bo = new FichierQuestionnairesBO("questionsQuizz_V1.1.csv", ';');
+        ServiceQuiz serv = new ServiceQuiz(bo);
+        serv.ajouterQuestionnaire(bo,1);
+        assertEquals(serv.FournirUnQuestionnaire(1),serv.fournirListeQuestionnaire().get(1));
     }
 }
